be395773a429d9990406e5ab7c92649cd3294a18
juanvillamizar96 <js.villamizar11@uniandes.edu.co>
2020-08-07 23:46:54 +0000

    Fetching Data from the API

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,17 +1,18 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 class App extends Component {
   state = {
     values: [],
   };
   componentDidMount() {
-    this.setState({
-      values: [
-        { id: 1, name: 'Value 101' },
-        { id: 2, name: 'Value 102' },
-      ],
+    axios.get('http://localhost:5000/api/values').then((response) => {
+      console.log(response);
+      this.setState({
+        values: response.data,
+      });
     });
   }
   render() {
