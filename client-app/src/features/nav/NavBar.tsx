58afb25307cd525c93aa79a12049253325bd888a
juanvillamizar96 <js.villamizar11@uniandes.edu.co>
2020-08-10 18:44:07 +0000

    Adding NavBar

---
+++ client-app/src/features/nav/NavBar.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Menu, Container, Button } from 'semantic-ui-react';
+import 'semantic-ui-css/semantic.min.css';
+
+const NavBar = () => {
+  return (
+    <Menu fixed='top' inverted>
+      <Container>
+        <Menu.Item header>
+          <img src='/assets/logo.png' alt='logo' />
+          Reactivities
+        </Menu.Item>
+        <Menu.Item name='Activities' />
+        <Menu.Item>
+          <Button positive content='Create Activity' />
+        </Menu.Item>
+      </Container>
+    </Menu>
+  );
+};
+
+export default NavBar;
