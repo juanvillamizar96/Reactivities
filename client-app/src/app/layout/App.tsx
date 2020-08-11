e2746f2e8c5e50c73cb8602872e1673f3a3df43c
juanvillamizar96 <js.villamizar11@uniandes.edu.co>
2020-08-11 18:04:42 +0000

    Set up MobX Store, use activityStore

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,4 +1,10 @@
-import React, { useState, useEffect, Fragment, SyntheticEvent } from 'react';
+import React, {
+  useState,
+  useEffect,
+  Fragment,
+  SyntheticEvent,
+  useContext,
+} from 'react';
 import { Container } from 'semantic-ui-react';
 import { IActivity } from '../models/activity';
 import NavBar from '../../features/nav/NavBar';
@@ -6,8 +12,10 @@ import 'semantic-ui-css/semantic.min.css';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
 import agent from '../api/agent';
 import LoadingComponent from './LoadingComponent';
+import ActivityStore from '../stores/activityStore';
 
 const App = () => {
+  const activityStore = useContext(ActivityStore);
   const [activities, setActivities] = useState<IActivity[]>([]);
   const [selectedActivity, setSelectedActivity] = useState<IActivity | null>(
     null
