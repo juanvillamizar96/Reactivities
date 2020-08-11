ecc24a1bcf9c569fe068388e9d203abf50b51c48
juanvillamizar96 <js.villamizar11@uniandes.edu.co>
2020-08-11 00:19:28 +0000

    Select individual activity, show them

--- client-app/src/features/activities/details/ActivityDetails.tsx
+++ client-app/src/features/activities/details/ActivityDetails.tsx
@@ -1,16 +1,24 @@
 import React from 'react';
 import { Card, Image, Icon, Button } from 'semantic-ui-react';
+import { IActivity } from '../../../app/models/activity';
+interface IProps {
+  activity: IActivity;
+}
 
-const ActivityDetails = () => {
+const ActivityDetails: React.FC<IProps> = ({ activity }) => {
   return (
     <Card fluid>
-      <Image src='/assets/placeholder.png' wrapped ui={false} />
+      <Image
+        src={`assets/categoryImages/${activity.category}.jpg`}
+        wrapped
+        ui={false}
+      />
       <Card.Content>
-        <Card.Header>title</Card.Header>
+        <Card.Header>{activity.title}</Card.Header>
         <Card.Meta>
-          <span>Date</span>
+          <span>{activity.date}</span>
         </Card.Meta>
-        <Card.Description>Description</Card.Description>
+        <Card.Description>{activity.description}</Card.Description>
       </Card.Content>
       <Card.Content extra>
         <Button.Group widths={2}>
